f891749f5e0d8e2730f49eb6324685e7461b2a4b
Henry Zhang <hzhang@iworkscorp.com>
2024-03-13 03:09:02 -0400

    attribute render and custom comp parameters

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de69f5e34c4e759d9f6d791cafb711707d695f0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8207463a4ad3ff1b9e5b1d36260d6d8e8c77a8ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
