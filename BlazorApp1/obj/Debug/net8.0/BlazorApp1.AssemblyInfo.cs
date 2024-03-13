0a0bd2fcef22e3a470f92b604548575d81fed650
Henry Zhang <hzhang@iworkscorp.com>
2024-03-13 03:41:59 -0400

    data, event, and code exe binding

--- BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8207463a4ad3ff1b9e5b1d36260d6d8e8c77a8ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f891749f5e0d8e2730f49eb6324685e7461b2a4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
